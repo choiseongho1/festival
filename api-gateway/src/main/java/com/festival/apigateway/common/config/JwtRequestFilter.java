582fb80e9327bcee34cce72fa3acdeff1667f1a5
SeongHo <95_csh@naver.com>
2024-10-26 14:57:33 +0900

    feat: user-service에서 발행 된 jwt토큰을 검증하는 jwtRequestFilter 추가

---
+++ api-gateway/src/main/java/com/festival/apigateway/common/config/JwtRequestFilter.java
@@ -0,0 +1,34 @@
+package com.festival.apigateway.common.config;
+
+import com.festival.apigateway.common.util.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        final String authorizationHeader = request.getHeader("Authorization");
+
+        String token = null;
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            token = authorizationHeader.substring(7);
+            if (jwtUtil.validateToken(token)) {
+                // 추가 인증 처리 로직
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
